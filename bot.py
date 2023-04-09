1bef214cd367ed05d920e3bf0645543bd4aba4ac
konichiwa55115 <121526988+konichiwa55115@users.noreply.github.com>
2023-04-09 08:05:59 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -18,13 +18,5 @@ def _telegram_file(client, message):
   try:
     file_path = message.download(file_name=DOWNLOAD_DIRECTORY)
     sent_message.edit(Messages.DOWNLOADED_SUCCESSFULLY.format(os.path.basename(file_path), humanbytes(os.path.getsize(file_path))))
-  with open('entry.mp3', 'wb') as f:
-      f.write(audio_file)
-    # Execute speech.py script with entry file
-  subprocess.call(['python', 'speech.py', 'RK3ETXWBJQSMO262RXPAIXFSG6NH3QRH', 'entry.mp3', 'transcription.txt'])
-    # Upload transcription file to user
-  with open('transcription.txt', 'rb') as f:
-      bot.send_document(message.chat.id, f)  
-    
-    
+
 bot.run()
