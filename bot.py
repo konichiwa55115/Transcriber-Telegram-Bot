150d5b5f7cc5ed50544a2f573d6436e114032512
konichiwa55115 <121526988+konichiwa55115@users.noreply.github.com>
2023-04-09 05:49:29 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,10 +10,10 @@ def handle_audio(message):
     # Download audio file
     audio_file = bot.download_file(bot.get_file(message.audio.file_id).file_path)
     # Rename audio file to "entry"
-    with open('entry', 'wb') as f:
+    with open('entry.mp3', 'wb') as f:
         f.write(audio_file)
     # Execute speech.py script with entry file
-    subprocess.getoutput(['python', 'speech.py', 'RK3ETXWBJQSMO262RXPAIXFSG6NH3QRH', 'entry', 'transcription.txt'])
+    subprocess.getoutput(['python', 'speech.py', 'RK3ETXWBJQSMO262RXPAIXFSG6NH3QRH', 'entry.mp3', 'transcription.txt'])
     # Upload transcription file to user
     with open('transcription.txt', 'rb') as f:
         bot.send_document(message.chat.id, f)
