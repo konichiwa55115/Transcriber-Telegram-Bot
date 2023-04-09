665be597c29bd46051028d2d5b2d9a38d573eec5
konichiwa55115 <121526988+konichiwa55115@users.noreply.github.com>
2023-04-09 08:38:41 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 from pyrogram import Client, filters
-bot = Clinet(
+bot = Client(
+    "myfirs",
     api_id=17983098,
     api_hash="ee28199396e0925f1f44d945ac174f64",
     bot_token="5714654934:AAFm0UBvzuU1X-Adg7QThWCzpoKBww9SNXE"
@@ -15,8 +16,7 @@ def _telegram_file(client, message):
   file = message.audio
   sent_message.edit(Messages.DOWNLOAD_TG_FILE.format(file.file_name, humanbytes(file.file_size), file.mime_type))
   LOGGER.info(f'Download:{user_id}: {file.file_id}')
-  try:
-    file_path = message.download(file_name=DOWNLOAD_DIRECTORY)
-    sent_message.edit(Messages.DOWNLOADED_SUCCESSFULLY.format(os.path.basename(file_path), humanbytes(os.path.getsize(file_path))))
+  file_path = message.download(file_name=DOWNLOAD_DIRECTORY)
+  sent_message.edit(Messages.DOWNLOADED_SUCCESSFULLY.format(os.path.basename(file_path), humanbytes(os.path.getsize(file_path))))
 
 bot.run()
