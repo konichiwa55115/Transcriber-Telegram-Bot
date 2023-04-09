f2891ca4c6936e83903337ec24e3bbde976a9f31
konichiwa55115 <121526988+konichiwa55115@users.noreply.github.com>
2023-04-09 07:27:39 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,28 +1,10 @@
-import telebot
-import subprocess
-
-# Replace with your Telegram Bot API token
-bot = telebot.TeleBot('5848326557:AAHvjj5sabP4XziObLnhv-EQNvm7X9S0Evw')
-
-@bot.message_handler(commands=['start'])
-def handle_start(message):
-    bot.reply_to(message, "السلام عليكم . أنا بوت تفريغ الصوتيات . فقط حول الصوتية التي تريد تفريغها \n\n لبقية البوتات هنا \n\n https://t.me/ibnAlQyyim/1120 ")
-
-
-# Handler for audio messages
-@bot.message_handler(content_types=['audio'])
-def handle_audio(message):
-    bot.reply_to(message, "جاري التفريغ . سيتم إرسال ملف التفريغ بعد الانتهاء إن شاء الله")
-    # Download audio file
-    audio_file = bot.download_file(bot.get_file(message.audio.file_id).file_path)
-    # Rename audio file to "entry"
-    with open('entry.mp3', 'wb') as f:
-        f.write(audio_file)
-    # Execute speech.py script with entry file
-    subprocess.call(['python', 'speech.py', 'RK3ETXWBJQSMO262RXPAIXFSG6NH3QRH', 'entry.mp3', 'transcription.txt'])
-    # Upload transcription file to user
-    with open('transcription.txt', 'rb') as f:
-        bot.send_document(message.chat.id, f)
-
-# Start the bot
-bot.polling()
+from pyrogram import Client, filters
+bot = Clinet(
+    api_id= ,
+    api_hash= ,
+    bot_token= 
+)
+@bot.on_message(filters.command('start') & filters.private)
+def command1(bot,message):
+    bot.send_message(message.chat.id, " السلام عليكم أنا بوت تفريغ صوتيات , فقط أرسل الصوتية هنا ")
+bot.run()
