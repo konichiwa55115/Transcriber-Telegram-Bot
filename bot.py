db0e866bf9584e0c88ee73fca81097a313143b89
konichiwa55115 <121526988+konichiwa55115@users.noreply.github.com>
2023-04-12 04:42:59 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,7 +8,9 @@ bot = Client(
 )
 @bot.on_message(filters.command('start') & filters.private)
 def command1(bot,message):
-    bot.send_message(message.chat.id, " السلام عليكم أنا بوت تفريغ صوتيات , فقط أرسل الصوتية هنا/n/n لبقية البوتات هنا \n\n https://t.me/ibnAlQyyim/1120 ")
+    bot.send_message(message.chat.id, " السلام عليكم أنا بوت تفريغ صوتيات , فقط أرسل الصوتية هنا\n\n  لبقية البوتات هنا \n\n https://t.me/ibnAlQyyim/1120 ")
+    disable_web_page_preview=True
+
     
 @bot.on_message(filters.private & filters.incoming & filters.audio )
 def _telegram_file(client, message):
