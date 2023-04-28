e55eefc4221bad56014f1a60807ef741aa80457c
konichiwa55115 <121526988+konichiwa55115@users.noreply.github.com>
2023-04-28 02:10:12 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,20 +5,20 @@ bot = Client(
     "myfirs",
     api_id=17983098,
     api_hash="ee28199396e0925f1f44d945ac174f64",
-    bot_token="5782497998:AAGQaYV5w_Rw1ZSBhApXgV763_g8wf_PZEw"
+    bot_token="6199159516:AAEB005tAqKx57Cp58BY3z2Ma3oMpSOe1dY"
 )
 @bot.on_message(filters.command('start') & filters.private)
 def command1(bot,message):
     bot.send_message(message.chat.id, " السلام عليكم أنا بوت تفريغ صوتيات , فقط أرسل الصوتية هنا\n\n  لبقية البوتات هنا \n\n https://t.me/ibnAlQyyim/1120 ",disable_web_page_preview=True)
     
-exists = os.path.isfile('/path/to/file')
+exists = os.path.isfile('/home/henrysawy51615/trans5115text/transcription.txt')
 @bot.on_message(filters.private & filters.incoming & filters.audio )
-def _telegram_file(client, message):
- 
+
   if exists:
         return
   else:
         pass
+def _telegram_file(client, message):
   user_id = message.from_user.id 
   sent_message = message.reply_text('جار التفريغ', quote=True)
   file = message.audio
