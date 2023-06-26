79d67c5b660516410cd60470346f4c604d8f734a
konichiwa55115 <121526988+konichiwa55115@users.noreply.github.com>
2023-06-26 06:32:04 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/konichiwa55115/trans5115text /LazyDeveloper
-cd /LazyDeveloper
+git clone https://github.com/konichiwa55115/Transcriber-Telegram-Bot /kony
+cd /kony
 pip3 install -r requirements.txt
 echo "Starting Bot...."
-gunicorn app:app & python3 bot.py
+python3 bot.py
