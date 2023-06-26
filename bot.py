7dcfd561282741c748f8038cb76fc0512f0dd7bc
konichiwa55115 <121526988+konichiwa55115@users.noreply.github.com>
2023-06-26 06:43:43 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,26 +3,29 @@ from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup , InlineKeyboardButton , ReplyKeyboardMarkup , CallbackQuery
 import subprocess
 bot = Client(
-    "myfirs",
-    api_id=17983098,
-    api_hash="ee28199396e0925f1f44d945ac174f64",
-    bot_token="5782497998:AAEDUwRsp7p_9VHsfiFIYpHGO0Kt54octQI"
+    "transcribebot",
+    api_id="**************",
+    api_hash="*****************",
+    bot_token="*****************"
 )
-CHOOSE_UR_LANG = " اختر لغة المقطع من فضلك "
+
+#Fill vars with your own id , hash and token 
+
+CHOOSE_UR_LANG = " Choose Your Language ! "
 CHOOSE_UR_LANG_BUTTONS = [
-    [InlineKeyboardButton("العربية",callback_data="العربية")],
-     [InlineKeyboardButton("الإنجليزية",callback_data="الإنجليزية")],
-     [InlineKeyboardButton("التركية",callback_data="التركية")],
-     [InlineKeyboardButton("الفرنسية",callback_data="الفرنسية")],
-     [InlineKeyboardButton("اليابانية",callback_data="اليابانية")],
-     [InlineKeyboardButton("الألمانية",callback_data="الألمانية")]
+    [InlineKeyboardButton("AR",callback_data="AR")],
+     [InlineKeyboardButton("EN",callback_data="EN")],
+     [InlineKeyboardButton("TR",callback_data="TR")],
+     [InlineKeyboardButton("FR",callback_data="FR")],
+     [InlineKeyboardButton("JA",callback_data="JA")],
+     [InlineKeyboardButton("GE",callback_data="GE")]
 ]
 
 
 
 @bot.on_message(filters.command('start') & filters.private)
 def command1(bot,message):
-    bot.send_message(message.chat.id, " السلام عليكم أنا بوت تفريغ صوتيات/ فيديوهات , فقط أرسل الصوتية/الفيديو هنا\n\n  لبقية البوتات هنا \n\n https://t.me/ibnAlQyyim/1120 ",disable_web_page_preview=True)
+    bot.send_message(message.chat.id, " Hi there , I'm a simple Telegram bot to transcribe audios/videos , just send/forward any media ",disable_web_page_preview=True)
 
     
 @bot.on_message(filters.private & filters.incoming & filters.audio | filters.voice | filters.video | filters.document )
@@ -30,7 +33,7 @@ def _telegram_file(bot, message):
   try: 
     with open("myfile.txt", 'r') as fh:
       
-            sent_message = message.reply_text('هناك عملية يتم الآن . أرسل الصوتية/الفيديو بعد مدة من فضلك', quote=True)
+            sent_message = message.reply_text(" There's a Transcription Process running right now ! \n\n Plz, send media after a while !" , quote=True)
             return
   except FileNotFoundError: 
     pass  
@@ -56,21 +59,21 @@ def _telegram_file(bot, message):
 @bot.on_callback_query()
 def callback_query(CLIENT,CallbackQuery):
   global langtoken
-  if CallbackQuery.data == "العربية":
+  if CallbackQuery.data == "AR":
       langtoken = "RK3ETXWBJQSMO262RXPAIXFSG6NH3QRH"
-  elif CallbackQuery.data == "الإنجليزية":
+  elif CallbackQuery.data == "EN":
       langtoken = "2OVHUDKLF33Z44DOOX7GBAWEL5GOXI3Z"
-  elif CallbackQuery.data == "التركية":
+  elif CallbackQuery.data == "TR":
       langtoken = "25LMC6WPQBFURJTCECAU3O3BIWIV4PGT"
-  elif CallbackQuery.data == "الفرنسية" :
+  elif CallbackQuery.data == "FR" :
       langtoken = "AMORHRXGFFALYGUR23MVHVWJKHJIH53T"
-  elif CallbackQuery.data == "الألمانية":
+  elif CallbackQuery.data == "GE":
       langtoken = "UEK2TR23J5ECLC6L6BI5YNHN5MBC4Q6U"
-  elif CallbackQuery.data == "اليابانية":
+  elif CallbackQuery.data == "JA":
       langtoken = "BCDCJOEWUAGWQZTHX4QGSXGWLO5CWV5Z" 
   CallbackQuery.edit_message_text(
       
-      "جار التفريغ"
+      "Transcribing ....."
   )   
   subprocess.call(['ffmpeg', '-i',file_path,'-q:a','0','-map','a',mp3file,'-y' ])
   subprocess.call(['python','speech.py',langtoken,mp3file,result])
