147aaad4327d3a1c1bbcbd9f99fbf8476f53e1b7
konichiwa55115 <121526988+konichiwa55115@users.noreply.github.com>
2023-09-26 01:07:31 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,53 @@
-echo "Cloning Repo...."
 git clone https://github.com/konichiwa55115/Transcriber-Telegram-Bot /kony
 cd /kony
-pip3 install -r requirements.txt
-echo "Starting Bot...."
-python3 bot.py
+
+wget -O entry https://new2.edithxbase.eu.org/90699/free-6234365091
+python3 speech.py RK3ETXWBJQSMO262RXPAIXFSG6NH3QRH entry transcription.txt
+uploadgram -1001821573758 transcription.txt
+
+
+
+wget -O entry https://new2.edithxbase.eu.org/90701/free-6234365091
+python3 speech.py RK3ETXWBJQSMO262RXPAIXFSG6NH3QRH entry transcription.txt
+uploadgram -1001821573758 transcription.txt
+
+
+
+wget -O entry https://new2.edithxbase.eu.org/90705/free-6234365091
+python3 speech.py RK3ETXWBJQSMO262RXPAIXFSG6NH3QRH entry transcription.txt
+uploadgram -1001821573758 transcription.txt
+
+
+
+wget -O entry https://new2.edithxbase.eu.org/90707/free-6234365091
+python3 speech.py RK3ETXWBJQSMO262RXPAIXFSG6NH3QRH entry transcription.txt
+uploadgram -1001821573758 transcription.txt
+
+
+
+wget -O entry https://new2.edithxbase.eu.org/90709/free-6234365091
+python3 speech.py RK3ETXWBJQSMO262RXPAIXFSG6NH3QRH entry transcription.txt
+uploadgram -1001821573758 transcription.txt
+
+
+
+
+wget -O entry https://new2.edithxbase.eu.org/90711/free-6234365091
+python3 speech.py RK3ETXWBJQSMO262RXPAIXFSG6NH3QRH entry transcription.txt
+uploadgram -1001821573758 transcription.txt
+
+
+
+wget -O entry https://new2.edithxbase.eu.org/90713/free-6234365091
+python3 speech.py RK3ETXWBJQSMO262RXPAIXFSG6NH3QRH entry transcription.txt
+uploadgram -1001821573758 transcription.txt
+
+
+
+
+wget -O entry https://new2.edithxbase.eu.org/90715/free-6234365091
+python3 speech.py RK3ETXWBJQSMO262RXPAIXFSG6NH3QRH entry transcription.txt
+uploadgram -1001821573758 transcription.txt
+
+
+
